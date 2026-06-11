1721f478279d9e16f90b8aa4561913a3a5bee933
prcht <pricheta@inbox.ru>
2026-06-11 14:14:29 +0800

    work in progress

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 from anyio.functools import lru_cache
 from fastapi import FastAPI, Depends, HTTPException
+from pydantic import Field
 from starlette import status
 
 from database import DatabaseClient, DatabaseClientConfig
@@ -36,17 +37,23 @@ async def mark_task_done(task_id: int, database_client: DatabaseClient = Depends
 
 
 @fastapi_app.get("/delay_task", status_code=status.HTTP_200_OK)
-async def delay_task(task_id: int, times: int, database_client: DatabaseClient = Depends(get_database_client)) -> None:
+async def delay_task(
+    task_id: int,
+    hours: int | None = Field(gt=0, default=None),
+    times: int | None = Field(gt=0, default=None),
+    database_client: DatabaseClient = Depends(get_database_client)
+) -> None:
     db_task = database_client.get_task(task_id)
     if not db_task:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     task = Task.model_validate(db_task, from_attributes=True)
 
-    if task.frequency_hours:
+    if hours:
+        task.next_time_to_do += timedelta(hours=hours)
+
+    if task.frequency_hours and times:
         for _ in range(times):
             task.next_time_to_do += timedelta(hours=task.frequency_hours)
-    else:
-        task.next_time_to_do += timedelta(hours=times)
 
     database_client.update_task_next_time_to_do(task_id=task.id, next_time_to_do=task.next_time_to_do)
